f6afd979d24b6e0ecab6fe8ad7c4371e5842455c
Taofiq Aiyelabegan <61933245+Taofiqq@users.noreply.github.com>
2022-04-18 01:59:58 +0800

    new commit

--- js/main.js
+++ js/main.js
@@ -85,3 +85,22 @@ function scrollActive() {
   });
 }
 window.addEventListener("scroll", scrollActive);
+
+// testimonial
+
+let swiperTestimonial = new Swiper(".testimonial__container ", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    },
+  },
+});
